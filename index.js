478c41592a49432f5dce1767619a5ed8bbb74f11
vayntail <113714157+vayntail@users.noreply.github.com>
2024-11-25 10:42:54 -0500

    Step 8: error middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// These are now route imports, not database imports!
 const users = require("./routes/users");
 const posts = require("./routes/posts");
+
+const error = require("./utilities/error");
 
 const app = express();
 const port = 3000;
@@ -39,16 +40,10 @@ app.use("/api", function (req, res, next) {
   var key = req.query["api-key"];
 
   // Check for the absence of a key.
-  if (!key) {
-    res.status(400);
-    return res.json({ error: "API Key Required" });
-  }
+  if (!key) next(error(400, "API Key Required"));
 
   // Check for key validity.
-  if (apiKeys.indexOf(key) === -1) {
-    res.status(401);
-    return res.json({ error: "Invalid API Key" });
-  }
+  if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
 
   // Valid key! Store it in req.key for route access.
   req.key = key;
@@ -64,9 +59,21 @@ app.get("/", (req, res) => {
 });
 
 // 404 Middleware
-app.use((req, res) => {
-  res.status(404);
-  res.json({ error: "Resource Not Found" });
+app.use((req, res, next) => {
+  next(error(404, "Resource Not Found"));
+});
+
+// Error-handling middleware.
+// Any call to next() that includes an
+// Error() will skip regular middleware and
+// only be processed by error-handling middleware.
+// This changes our error handling throughout the application,
+// but allows us to change the processing of ALL errors
+// at once in a single location, which is important for
+// scalability and maintainability.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
 });
 
 app.listen(port, () => {
