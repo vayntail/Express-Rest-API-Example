e69ef7c8a7e877ab17f1a8d63ee598960c27f847
vayntail <113714157+vayntail@users.noreply.github.com>
2024-11-25 10:15:54 -0500

    Step two

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const port = 3000;
 const users = require("./data/users");
 const posts = require("./data/posts");
 
+// Creating a GET route for the entire users database.
+// This would be impractical in larger data sets.
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+// Creating a simple GET route for individual users,
+// using a route parameter for the unique id.
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id == req.params.id);
+  if (user) res.json(user);
+});
+
+// Creating a GET route for the entire posts database.
+// This would be impractical in larger data sets.
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+// Creating a simple GET route for individual posts,
+// using a route parameter for the unique id.
+app.get("/api/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id == req.params.id);
+  if (post) res.json(post);
+});
+
 app.get("/", (req, res) => {
   res.send("Work in progress!");
 });
